c2dafce305782ba77ab2f47d3763de20183aed6e
Gaby <gabriela.valderrama@securex.pe>
2022-11-29 11:10:20 -0500

    avances login

---
+++ src/main/java/com/cibertec/Controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.cibertec.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+
+    @RequestMapping(value = {"","/login"},method = RequestMethod.GET)
+    public String login(Model model){
+        model.addAttribute("log","log");
+        return "login";
+    }
+
+}
